7e38acae0feeecdfa9d86103d45dd76395675d92
zehh123 <zehaayesha906@gmail.com>
2026-09-15 08:50:54 -0500

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,22 @@
+def chatbot():
+    print("Welcome to Basic Chatbot!")
+    print("You can say: hello, how are you, bye")
+
+    while True:
+        user_input = input("You: ").lower()
+
+        if user_input == "hello":
+            print("Chatbot: Hi!")
+
+        elif user_input == "how are you":
+            print("Chatbot: I'm fine, thanks!")
+
+        elif user_input == "bye":
+            print("Chatbot: Goodbye!")
+            break
+
+        else:
+            print("Chatbot: Sorry, I don't understand that.")
+
+
+chatbot()
